e9a2dffbbddbf32555cfed93f01f32bd8e6a7f44
Graham Francisco <42788240+gbfrancisco@users.noreply.github.com>
2020-07-22 18:00:27 -1000

    Updates

---
+++ precache-manifest.ed5fcb4ac3cc356d4b0668310c7d2fc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a34f0222b25c2cbae2b13d9d60bc162",
+    "url": "/testingbullshit/index.html"
+  },
+  {
+    "revision": "12d2c6d1f9a383a6540f",
+    "url": "/testingbullshit/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d2a5cb8fd95a5216e57",
+    "url": "/testingbullshit/static/js/2.e6d5e801.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testingbullshit/static/js/2.e6d5e801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12d2c6d1f9a383a6540f",
+    "url": "/testingbullshit/static/js/main.3b37ab95.chunk.js"
+  },
+  {
+    "revision": "de2037de87320901db6b",
+    "url": "/testingbullshit/static/js/runtime-main.ea08dcd2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testingbullshit/static/media/logo.5d5d9eef.svg"
+  }
+]);
